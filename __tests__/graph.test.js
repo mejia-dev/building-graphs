e48844eff9091076beff85717c074e022cf743d6
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-05 10:16:15 -0800

    Add project

---
+++ __tests__/graph.test.js
@@ -0,0 +1,58 @@
+import Graph from '../src/graph.js';
+
+describe('Graph', () => {
+
+  let graph = new Graph();
+
+  afterEach(() => {
+    graph = new Graph();
+  });
+
+  test('should correctly instantiate a graph', () => {
+    expect(graph.adjacencyList.size).toEqual(0);
+  });
+
+  test('should add a new node', () => {
+    graph.addNode("Jasmine");
+    expect(graph.adjacencyList.get("Jasmine").size).toEqual(0);
+  });
+
+  test('should return false if the node does not exist in the adjacency list', () => {
+    expect(graph.hasNode("Ada")).toEqual(false);
+  });
+
+  test('check to see if node exists in graph', () => {
+    graph.addNode("Jasmine");
+    expect(graph.hasNode("Jasmine")).toEqual(true);
+  });
+
+  test('add an edge between two nodes', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    graph.createEdge("Jasmine", "Ada");
+    expect(graph.adjacencyList.get("Jasmine").has("Ada")).toEqual(true);
+    expect(graph.adjacencyList.get("Ada").has("Jasmine")).toEqual(true);
+  });
+
+  test('check to see if edge exists in graph', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    expect(graph.hasEdge("Jasmine", "Ada")).toEqual(false);
+  });
+
+  test('add an edge between two nodes', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    graph.createEdge("Jasmine", "Ada");
+    expect(graph.hasEdge("Ada", "Jasmine")).toEqual(true);
+  });
+
+  test('remove an edge between two nodes', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    graph.createEdge("Jasmine", "Ada");
+    graph.removeEdge("Jasmine", "Ada");
+    expect(graph.hasEdge("Ada", "Jasmine")).toEqual(false);
+  });
+
+});
